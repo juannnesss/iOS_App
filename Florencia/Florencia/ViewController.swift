c1d0b24cdd2e315ca3e1b790983b7be67f4dcb9d
juannnesss <js.vargas2128@uniandes.edu.co>
2018-12-03 10:30:43 -0500

    ImageViewUpdates

--- Florencia/Florencia/ViewController.swift
+++ Florencia/Florencia/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
    
     
     //MARK: Properties
@@ -16,6 +16,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var btGo: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     
     
     override func viewDidLoad() {
@@ -28,7 +29,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-    //MARK: Delegados de UIFieldDelegate
+    //MARK: Delegados de UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
       //hide the keyboard
         textField.resignFirstResponder()
@@ -37,11 +38,40 @@ class ViewController: UIViewController,UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         lbName.text=textField.text
     }
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        //se activa cuando se cancela la selecion de pohotos, no se hace nada dismiss
+        dismiss(animated: true, completion:nil)
+    }
+    //se maneja lo que se hace con la infomracion selecionada
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        //set imageView to display the selected image
+        imageView.image=selectedImage
+        //dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     
     //MARK: Actions
     
   
-
+// metodo para selecionar la photo, se agrego UIImagePickerControllerDelegate,UINavigationControllerDelegate
+    //protocolos
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //hide the keyboard
+        txtName.resignFirstResponder()
+        //UIImagePickerController permite seleccionar fotos desde la libreria
+        let imagePickerControler=UIImagePickerController()
+        //para que solo se puedan selecionar, no tomar
+        imagePickerControler.sourceType = .photoLibrary
+        //Avisa a ViewController sea notifcado
+        imagePickerControler.delegate=self
+        present(imagePickerControler,animated: true,completion: nil)
+    }
+   
     @IBAction func setGo(_ sender: UIButton) {
         lbName.text="New Go"
     }
